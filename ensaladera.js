aacdc6f066431f33f6ed423cf585fbc9ea26447e
LeonardoGarcia-Estralagio <133721858+LeonardoGarcia-Estralagio@users.noreply.github.com>
2023-05-22 20:15:26 -0300

    Update ensaladera.js

--- ensaladera.js
+++ ensaladera.js
@@ -1,1 +1,8 @@
 console.log("Aquí esta la ensaladera")
+console.log("Instucciones")
+console.log(`
+1. lavar verduras
+2. cortar a mano la lechuga
+3. cortar el tomate en rodajas, a 45° grados
+4. cortar la cebolla a la juliana
+`)
