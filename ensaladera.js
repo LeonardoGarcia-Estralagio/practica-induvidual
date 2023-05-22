44e07099198ba8ced4ec4e8371ef2ab3f5e513d5
LeonardoGarcia-Estralagio <estralagio@gmail.com>
2023-05-22 20:19:00 -0300

    agrega instrucciones

--- ensaladera.js
+++ ensaladera.js
@@ -1,1 +1,8 @@
 console.log("Aquí esta la ensaladera")
+console.log("Instucciones")
+console.log(`
+1. lavar las verduras
+2. cortar a cuchillo la lechuga
+3. cortar el tomate en cuadrados
+4. cortar la cebolla en rodajas
+`)
